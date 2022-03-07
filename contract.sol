474ee66df175b878dda42208d0ae26933a774cf2
fayçal ben <bensassi.faysel@gmail.com>
2022-03-07 21:41:21 +0000

    Copied the token code into the token.sol file

--- contract.sol
+++ contract.sol
@@ -1,24 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-contract SimpleStorage {
-  uint256 storedData;
-	
-  function get() public view returns (uint) {
-    return storedData;
-  }
+// This example was taken from the code examples on:
+// https://ethereum.org/en/
 
-  function set(uint x) public {
-    storedData = x;
-  }
+/**
+NOTE: THIS WILL NOT BE AUTOMATICALLY COMPILED.
+If you want it to compile, either import it into contract.sol or copy and paste the contract directly into there!
+**/
 
-  function double() public {
-    storedData *= 2;
-  }
-}
+contract SimpleToken {
+    // An address is comparable to an email address - it's used to identify an account on Ethereum.
+    address public owner;
+    uint256 public constant token_supply = 1000000000000;
 
-contract MathTest {
-	function multiply(uint a, uint b) public pure returns (uint) {
-    return a*b;
-  }
+    // A mapping is essentially a hash table data structure.
+    // This mapping assigns an unsigned integer (the token balance) to an address (the token holder).
+    mapping (address => uint) public balances;
+
+
+  // When 'SimpleToken' contract is deployed:
+  // 1. set the deploying address as the owner of the contract
+  // 2. set the token balance of the owner to the total token supply
+    constructor() {
+        owner = msg.sender;
+        balances[owner] = token_supply;
+    }
+
+    // Sends an amount of tokens from any caller to any address.
+    function transfer(address receiver, uint amount) public {
+        // The sender must have enough tokens to send
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+
+        // Adjusts token balances of the two addresses
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
 }
